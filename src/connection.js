622eecfaccc9540562815e2179491a2259ebc269
Joan Sebastian Riveros Lozada <joanriveros96@gmail.com>
2020-04-20 00:32:27 -0500

    new app
    nueva aplicacion

--- src/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://admin:YourWork@cluster0-mcpbb.mongodb.net/test?retryWrites=true&w=majority',{
- 
-    useNewUrlPaerser: true,
-    useUnifiedTopology: true, 
-    useCreateIndex: true
-
-}).then(db => console.log('connection established succesfully'))
--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-require('./connection');
-
-async function init(){
-    await app.listen(3000);
-    console.log('server on localhost: 3000')
-}
-
-init();
